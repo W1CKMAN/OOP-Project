10658cda16261ef4aa5920429b0e4d2eeebd0a80
iamhelitha <helithalochana@gmail.com>
2024-01-14 21:38:50 +0530

    made the connection with database

--- src/main/java/DatabaseConnection/DatabaseConnection.java
+++ src/main/java/DatabaseConnection/DatabaseConnection.java
@@ -1,4 +1,19 @@
 package DatabaseConnection;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DatabaseConnection {
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/oop-chaos";
+    private static final String DATABASE_USER = "root";
+    private static final String DATABASE_PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
