b92972b949bca99b372d37d5b2c0dd472e35f1af
senuja2003 <156533451+senuja2003@users.noreply.github.com>
2024-01-15 17:56:31 +0530

    Update Inventory.java

--- src/main/java/Models/Inventory.java
+++ src/main/java/Models/Inventory.java
@@ -1,4 +1,49 @@
 package Models;
 
 public class Inventory {
+    private int productId;
+    private String productName;
+    private int quantity;
+    private double price;
+
+    public Inventory() {
+    }
+
+    public Inventory(String productName, int quantity, double price) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
